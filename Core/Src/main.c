3bd5e4865d711becbbcfa51a31cb22b3ec83756c
RobertZubek <robert.zubek.ofc@gmail.com>
2025-06-23 02:43:54 +0200

    LoRa send and receive

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -47,22 +47,25 @@
 /* USER CODE BEGIN PV */
 extern UART_HandleTypeDef huart2;
 extern SPI_HandleTypeDef hspi1;
+
 LoRa lora;
+
 char uart_rx_buffer[RX_BUFFER_SIZE];
 uint8_t uart_rx_index = 0;
 uint8_t uart_rx_byte;
+
+uint8_t lora_rx_buffer[RX_BUFFER_SIZE];
+uint8_t lora_rx_length = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-
+void Error_Handler(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-void Error_Handler(void);
-
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
     if (huart->Instance == USART2) {
         if (uart_rx_index < RX_BUFFER_SIZE - 1) {
@@ -74,14 +77,12 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
                 uart_rx_buffer[uart_rx_index++] = uart_rx_byte;
             }
         } else {
-            uart_rx_index = 0; 
+            uart_rx_index = 0;
         }
 
-        HAL_UART_Receive_IT(&huart2, &uart_rx_byte, 1); 
+        HAL_UART_Receive_IT(&huart2, &uart_rx_byte, 1);
     }
 }
-
-
 /* USER CODE END 0 */
 
 /**
@@ -90,31 +91,13 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
   */
 int main(void)
 {
-
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
   /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_SPI1_Init();
   MX_USART2_UART_Init();
+
   /* USER CODE BEGIN 2 */
   lora.hspi = &hspi1;
   lora.nss_port = CS_LORA_GPIO_Port;
@@ -123,21 +106,29 @@ int main(void)
   lora.reset_pin = LORA_RST_Pin;
 
   if (!LoRa_Init(&lora)) {
-      Error_Handler(); 
+      Error_Handler();
   }
-  HAL_UART_Receive_IT(&huart2, &uart_rx_byte, 1);
 
+  LoRa_ReceiveContinuous(&lora);
+
+  HAL_UART_Receive_IT(&huart2, &uart_rx_byte, 1);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
+    lora_rx_length = LoRa_ReceivePacket(&lora, lora_rx_buffer, RX_BUFFER_SIZE);
 
-    /* USER CODE BEGIN 3 */
+    if (lora_rx_length > 0) {
+        HAL_UART_Transmit(&huart2, lora_rx_buffer, lora_rx_length, HAL_MAX_DELAY);
+        char newline[2] = "\r\n";
+        HAL_UART_Transmit(&huart2, (uint8_t*)newline, 2, HAL_MAX_DELAY);
+    }
+
+    HAL_Delay(10);
   }
-  /* USER CODE END 3 */
+  /* USER CODE END WHILE */
 }
 
 /**
@@ -149,14 +140,9 @@ void SystemClock_Config(void)
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -166,8 +152,6 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
@@ -181,38 +165,20 @@ void SystemClock_Config(void)
   }
 }
 
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
 /**
   * @brief  This function is executed in case of error occurrence.
   * @retval None
   */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
   while (1)
   {
   }
-  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
